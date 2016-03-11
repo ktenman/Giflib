2ad763b8cae70246dbb697e7b42e93941fecb2b2
Konstantin <tenman@tlu.ee>
2016-03-12 01:29:10 +0200

    Thymeleaf updated to 1.3.3 Added templates

--- src/main/java/eu/estimol/giflib/controllers/GifController.java
+++ src/main/java/eu/estimol/giflib/controllers/GifController.java
@@ -7,14 +7,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class GifController {
     @RequestMapping("/")
-    @ResponseBody
     public String listGifs(){
-        return "List of all the GIFs!";
-    }
-    @RequestMapping("/test")
-    @ResponseBody
-    public String test(){
-        return "test";
+        return "home";
     }
 
 }
