1f5450c19e22a856725a7463cb31d87014e58e90
ktenman <ktenman@gmail.com>
2016-03-12 19:03:39 +0200

    Fixed GifController

--- src/main/java/eu/estimol/giflib/controller/GifController.java
+++ src/main/java/eu/estimol/giflib/controller/GifController.java
@@ -5,10 +5,7 @@ import eu.estimol.giflib.model.Gif;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.time.LocalDate;
-import java.util.Arrays;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class GifController {
@@ -21,9 +18,19 @@ public class GifController {
         return "home";
     }
 
-    @RequestMapping("/gif")
-    public String gifDetails (ModelMap modelMap){
-        Gif gif = new Gif("compiler-bot", LocalDate.of(2016,2,13),"Konstantin Tenman", true);
+//    @RequestMapping("/gif")
+//    public String gifDetails (ModelMap modelMap){
+//        Gif gif = new Gif("compiler-bot", LocalDate.of(2016,2,13),"Konstantin Tenman", true);
+//        modelMap.put("gif",gif);
+//        return "gif-details";
+//    }
+
+    @RequestMapping(value = "/gif/{gifName}", method = RequestMethod.GET)
+    public String gifDetailsByName (ModelMap modelMap, @PathVariable String gifName){
+        Gif gif = gifRepository.findByName(gifName);
+        if (gif==null){
+            return "home";
+        }
         modelMap.put("gif",gif);
         return "gif-details";
     }
