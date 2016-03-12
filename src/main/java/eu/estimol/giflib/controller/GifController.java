4b2672691c2953ec190edc5513e4e94afb059439
ktenman <ktenman@gmail.com>
2016-03-12 15:02:28 +0200

    Added ModalMap

--- src/main/java/eu/estimol/giflib/controller/GifController.java
+++ src/main/java/eu/estimol/giflib/controller/GifController.java
@@ -1,13 +1,29 @@
 package eu.estimol.giflib.controller;
 
+import eu.estimol.giflib.model.Gif;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
+import java.util.Arrays;
 
 @Controller
 public class GifController {
     @RequestMapping("/")
     public String listGifs(){
         return "home";
+    }
+
+    @RequestMapping("/gif")
+    public String gifDetails (ModelMap modelMap){
+        Gif gif = new Gif("compiler-bot", LocalDate.of(2016,2,13),"Konstantin Tenman", true);
+        modelMap.put("gif",gif);
+        System.out.println(modelMap.toString());
+        for (int i = 0; i < modelMap.size(); i++) {
+            System.out.println(modelMap.get(i).toString());
+        }
+        return "gif-details";
     }
 
 }
