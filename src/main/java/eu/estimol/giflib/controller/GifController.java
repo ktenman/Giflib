9ca4c8b17752fac258ff56ea0db0a5e1c6aa0602
ktenman <ktenman@gmail.com>
2016-03-12 14:52:38 +0200

    Added model

--- src/main/java/eu/estimol/giflib/controller/GifController.java
+++ src/main/java/eu/estimol/giflib/controller/GifController.java
@@ -1,4 +1,4 @@
-package eu.estimol.giflib.controllers;
+package eu.estimol.giflib.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
